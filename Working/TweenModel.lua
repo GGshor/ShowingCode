bfb00c57e5cbdffecadb1085ddc38f2efc0a96d8
GGshor <73340248+GGshor@users.noreply.github.com>
2021-07-17 15:45:44 +0200

    Goal accepts instance + Backwards compatibility fix

--- Working/TweenModel.lua
+++ Working/TweenModel.lua
@@ -38,10 +38,12 @@ Tween:Play() -- Plays the tween.
 local TweenModel = {}
 
 local expectgot = "%s expected, got %s"
-local TweenService = game:GetService("TweenService")
+local Tween = game:GetService("TweenService")
 
+local warn = function(Data) for _,m in pairs({Data}) do warn(" { Tween-Model Module } -- " .. tostring(m)) end end
+local print = function(Data) for _,m in pairs({Data}) do print(" { Tween-Model Module } -- " .. tostring(m)) end end
 
-function TweenModel.new(Model,TweenInfo,Goal)
+function TweenModel.new(Model,Info,Goal)
 	local IsModel, _  = pcall(function()
 		if Model.PrimaryPart then
 			return
@@ -50,62 +52,84 @@ function TweenModel.new(Model,TweenInfo,Goal)
 		end
 	end)
 
+	local IsCFrame, CheckedCFrame = pcall(function()
+		if typeof(Goal) == "CFrame" then
+			return Goal
+		else
+			if Goal.CFrame then
+				return Goal.CFrame
+			end
+		end
+	end)
+
 	if IsModel == false then
 		warn(expectgot:format("Model",typeof(Model)))
 		return
 	end
-	if typeof(Goal) ~= "CFrame" then
-		warn(expectgot:format("CFrame",typeof(Goal)))
+
+	if IsCFrame == false then
+		warn(expectgot:format("CFrame/Instance",typeof(Goal)))
 		return
 	end
 
-	if typeof(TweenInfo) ~= "TweenInfo" then
-		warn(expectgot:format("TweenInfo",typeof(TweenInfo)))
+	if typeof(Info) ~= "TweenInfo" then
+		warn(expectgot:format("TweenInfo",typeof(Info)))
 		return
 	end
 
 	local CF = Instance.new("CFrameValue")
 	CF.Value = Model:GetPrimaryPartCFrame()
-
-	CF:GetPropertyChangedSignal('Value'):Connect(function()
+	CF.Changed:Connect(function()
 		Model:SetPrimaryPartCFrame(CF.Value)
 	end)
 
-	local CreatedTween = TweenService:Create(CF,TweenInfo,{Value = Goal})
+	local CreatedTween = Tween:Create(CF,Info,{Value = CheckedCFrame})
 
 	return CreatedTween
 end
 
 function TweenModel:Play()
-	error("Deprecated method of using TweenModel module detected, please update your code!",0)
+	error(" { Tween-Model Module } -- Deprecated method of using TweenModel module detected, please update your code!",0)
 end
 
 function TweenModel:PlayInstant(Model,Goal,Duration,Style,Direction,Repeat,Reverses,Delay)
-	warn("\n-------------------------\n\nDeprecated method of using TweenModule detected, please update your code!\n\nBackwards compatibility may NOT work with your code!\n\n-------------------------")
+	if typeof(Goal) ~= "TweenInfo" then -- If goal isn't using the new way then use deprecated method
+		if Duration == nil then
+			Duration = 1
+		end
+		if Style == nil then
+			Style = Enum.EasingStyle.Quad
+		end
+		if Direction == nil then
+			Direction = Enum.EasingDirection.Out
+		end
+		if Repeat == nil then
+			Repeat = 0
+		end
+		if Reverses == nil then
+			Reverses = false
+		end
+		if Delay == nil then
+			Delay = 0
+		end
 
-	if Duration == nil then
-		Duration = 1
-	end
-	if Style == nil then
-		Style = Enum.EasingStyle.Quad
-	end
-	if Direction == nil then
-		Direction = Enum.EasingDirection.Out
-	end
-	if Repeat == nil then
-		Repeat = 0
-	end
-	if Reverses == nil then
-		Reverses = false
-	end
-	if Delay == nil then
-		Delay = 0
-	end
-	
-	local info = TweenInfo.new(Duration,Style,Direction,Repeat,Reverses,Delay)
+		local info = TweenInfo.new(Duration,Style,Direction,Repeat,Reverses,Delay)
 
-	TweenModel.new(Model,info,Goal.CFrame):Play()
+		TweenModel.new(Model,info,Goal.CFrame):Play()
+		
+	else -- Is using the new way
+		local IsCFrame, CheckedCFrame = pcall(function()
+			if typeof(Duration) == "CFrame" then
+				return Duration
+			else
+				if Duration.CFrame then
+					return Duration.CFrame
+				end
+			end
+		end)
 
+		TweenModel.new(Model,Goal,CheckedCFrame):Play()
+	end
 end
 
 return TweenModel
